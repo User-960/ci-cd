16c59ca5d5488721ed34c761e3f87a6c2dfadaa9
Ivan <ivanvolodin960@gmail.com>
2023-08-07 14:31:38 +0300

    feat: add counter

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,16 +1,25 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Inter } from 'next/font/google'
 import Layout from '../../layout/Layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
 const Home: FC = () => {
+  const [state, setState] = useState<number>(0)
+  const increment = () => setState(prev => prev + 1)
+
   return (
     <Layout>
       <h2 data-testid="title-docs">
         Docs <span>-&gt;</span>
       </h2>
       <p>Test CI/CD</p>
+      <div>
+        <h3>Value = {state}</h3>
+        <button onClick={increment}>
+          Increment
+        </button>
+      </div>
     </Layout>
   )
 }
